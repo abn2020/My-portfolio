6f60cd2fe34f08a3cbf90ea3843e9de1449faa49
Abner Martins <74722266+abn2020@users.noreply.github.com>
2023-01-09 17:40:47 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import HomePage from '../../my-portfolio/src/components/templates/Home'
+import HomePage from '../../my-portfolio/src/components/templates/Home/index'
 
 export default function Home() {
   return (
